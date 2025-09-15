59528544d41c63fc75db8b148dd28294afa72a45
Leejaeyun123 <qazxczf@gmail.com>
2025-09-15 17:55:56 +0900

    09-15

--- Java/src/main/java/com/example/Hello.java
+++ Java/src/main/java/com/example/Hello.java
@@ -2,16 +2,8 @@ package com.example;
 
 public class Hello {
     public static void main(String args[]) {
-        String studentName = "John Doe";
-        int studentID = 15;
-        int studentAge = 23;
-        float studentFee = 75.25f;
-        char studentGrade = 'B';
-
-        System.out.println("Student name : " + studentName);
-        System.out.println("Student id: " + studentID);
-        System.out.println("Student age : " + studentAge);
-        System.out.println("Student fee : " + studentFee);
-        System.out.println("Student grade : " + studentGrade);
+        String txt = "                          Hello World             ";
+        System.out.println("Before: [" + txt + "]");
+        System.out.println("After: [" + txt.trim() + "]");
     }
 }
