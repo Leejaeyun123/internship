1189aafd588afd34b4bf0ec2510c83de5691a3fd
Leejaeyun123 <qazxczf@gmail.com>
2025-09-17 17:53:31 +0900

    09-17

--- Java/src/main/java/com/example/Hello.java
+++ Java/src/main/java/com/example/Hello.java
@@ -1,9 +1,13 @@
 package com.example;
 
 public class Hello {
-    public static void main(String args[]) {
-        String txt = "                          Hello World             ";
-        System.out.println("Before: [" + txt + "]");
-        System.out.println("After: [" + txt.trim() + "]");
+    static void myMethod(String a, int b) {
+        System.out.println(a + " is " + b);
+    }
+
+    public static void main(String[] args) {
+        myMethod("A", 15);
+        myMethod("B", 16);
+        myMethod("C", 17);
     }
 }
